35fd48a3cd3f3dfc847ab0214f9342613f741fad
sjf <sjf@juicyminds.cc>
2020-08-31 18:25:23 +0800

    修改时间间隔，更新收包地址

--- test.py
+++ test.py
@@ -1,13 +1,33 @@
-import pandas as pd
+import sys
+import random
+from PySide2 import QtCore, QtWidgets, QtGui
 
-import math
-import os
-import pandas
+class MyWidget(QtWidgets.QWidget):
+    def __init__(self):
+        super().__init__()
 
-c = True
+        self.hello = ["Hallo Welt", "Hei maailma", "Hola Mundo", "Привет мир"]
 
-if c:
-    pass
-else:
-    print('else')
+        self.button = QtWidgets.QPushButton("Click me!")
+        self.text = QtWidgets.QLabel("Hello World")
+        self.text.setAlignment(QtCore.Qt.AlignCenter)
 
+        self.layout = QtWidgets.QVBoxLayout()
+        self.layout.addWidget(self.text)
+        self.layout.addWidget(self.button)
+        self.setLayout(self.layout)
+
+        self.button.clicked.connect(self.magic)
+
+
+    def magic(self):
+        self.text.setText(random.choice(self.hello))
+
+if __name__ == "__main__":
+    app = QtWidgets.QApplication([])
+
+    widget = MyWidget()
+    widget.resize(800, 600)
+    widget.show()
+
+    sys.exit(app.exec_())
